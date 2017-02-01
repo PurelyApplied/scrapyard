ffaff368eae771522b20388324e719df159f26e8
Patrick Rhomberg <prhomberg@pivotal.io>
2017-01-31 16:50:07 -0800

    Made prefix_length variable, for adjustment on larger screens.

--- bin/short_pwd.py
+++ bin/short_pwd.py
@@ -2,14 +2,15 @@ import os
 from socket import gethostname
 hostname = gethostname()
 username = os.environ['USER']
+prefix_length = 30
 try:
     pwd = os.getcwd()
 except:
     pwd = "a/deleted/dir?"
 homedir = os.path.expanduser('~')
 pwd = pwd.replace(homedir, '~', 1)
-if len(pwd) > 30:
-    pwd = pwd[:10]+'...'+pwd[-20:] # first 10 chars+last 20 chars
+if len(pwd) > prefix_length:
+    pwd = pwd[:10]+'...'+pwd[-(prefix_length - 10):]
 print('[%s[@here]:%s/]$ ' % (username,
                              #hostname,
                              pwd))
