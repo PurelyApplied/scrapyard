6495da3c2cdbaee0b81a76330f7fd4d4bca673f4
Patrick Rhomberg @ home <purelyapplied@gmail.com>
2018-12-21 18:44:45 -0800

    Old attempt at a music organizer found in my music library.  Should actually flesh that out.

---
+++ misc_python/music_organizer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import os
+
+def get_extensions(start='./'):
+    extensions = set()
+    raw = set()
+    for root, dirs, files in os.walk(start):
+        level = root.replace(start, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        #print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            #print('{}{}'.format(subindent, f))
+            if '.' in f:
+                extensions.add(f.split('.')[-1])
+            else:
+                print("File {!r} has no extension.".format(f))
+                raw.add('{}/{}'.format(root, f))
+    return extensions, raw
+
+def identify_bad_folders(start, target_extensions, case_sensitive=False):
+    bad = set()
+    bad_but_with_sub = set()
+    for root, dirs, files in os.walk(start):
+        level = root.replace(start, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        #print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        print("Folder: {}".format(root))
+        print("Has subdirectories: {}".format(dirs))
+        if not any(
+                any(ext == f[-len(ext):]
+                    or (not case_sensitive and
+                        ext.lower() == f[-len(ext):].lower())
+                    for f in files)
+                for ext in target_extensions):
+            print("BAD?")
+            print("\n".join("    {}".format(f) for f in files))
+            if dirs:
+                bad_but_with_sub.add(root)
+            else:
+                print("Bad!")
+                bad.add(root)
+    return bad, bad_but_with_sub
